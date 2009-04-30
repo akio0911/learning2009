cf553243a91827d38f1acfacbb86fb8231a72cf5
akio0911 <akio0911@gmail.com>
2009-04-30 17:20:45 +0900

    Singletonに複数の実装を追加

--- lang/objective-c/Singleton/Singleton.m
+++ lang/objective-c/Singleton/Singleton.m
@@ -1,5 +1,6 @@
 @implementation Singleton
-
+/* 
+// マルチスレッドを考慮していない方法
 + (id)instance
 {
     static Singleton* _instance = nil;
@@ -8,5 +9,30 @@
     }
     return _instance;
 }
+*/
+// マルチスレッドを考慮した方法
+/*
+static Singleton* _instance = nil;
 
++ (void)load // クラスがランタイムに読み込まれるときに呼ばれる
+{
+    if(!_instance){
+	_instance = [[Singleton alloc] init];
+    }
+}
+
++ (id)instance
+{
+    return _instance;
+}
+*/
+// サブクラスでの拡張を考慮した方法
++ (id)instance
+{
+    static id _instance = nil;
+    if(!_instance){
+	_instance = [[self alloc] init];
+    }
+    return _instance;
+}
 @end
