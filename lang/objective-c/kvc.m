d0a7bede6171d5b5055eb784826ca5003f977567
akio0911 <akio0911@gmail.com>
2009-06-02 00:13:58 +0900

    インデント修正

--- lang/objective-c/kvc.m
+++ lang/objective-c/kvc.m
@@ -5,20 +5,20 @@ int main(int argc, char** argv) {
 
     id path = [NSString stringWithCString:argv[1]
 			encoding:NSASCIIStringEncoding];
-   id data = [NSData dataWithContentsOfFile:path];
+    id data = [NSData dataWithContentsOfFile:path];
     id plist = [NSPropertyListSerialization 
 		   propertyListFromData:data
 		   mutabilityOption: NSPropertyListMutableContainers
 		   format: NULL errorDescription:nil];
-   id aSet = [NSSet setWithArray:plist];
-
-   NSLog(@"1: %@\n", [plist valueForKey:@"division"]);
-   NSLog(@"2: %@\n", [aSet valueForKey:@"division"]);
-   NSLog(@"3: %@\n", [plist valueForKeyPath:@"list.name"]);
-   NSLog(@"4: %@\n", [plist valueForKeyPath:@"list.requests.device"]);
-   [plist setValue:@"10" forKeyPath:@"list.requests.price"];
-   NSLog(@"5: %@\n", [plist valueForKeyPath:@"list.requests"]);
-
+    id aSet = [NSSet setWithArray:plist];
+    
+    NSLog(@"1: %@\n", [plist valueForKey:@"division"]);
+    NSLog(@"2: %@\n", [aSet valueForKey:@"division"]);
+    NSLog(@"3: %@\n", [plist valueForKeyPath:@"list.name"]);
+    NSLog(@"4: %@\n", [plist valueForKeyPath:@"list.requests.device"]);
+    [plist setValue:@"10" forKeyPath:@"list.requests.price"];
+    NSLog(@"5: %@\n", [plist valueForKeyPath:@"list.requests"]);
+    
     [pool release];
     return 0;
 }
